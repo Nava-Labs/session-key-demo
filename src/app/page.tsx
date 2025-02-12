f0ca8e792c367a7ba9bc4f43df2dfe3376e868b8
keivnjs <kjsurtan@gmail.com>
2025-02-12 18:13:41 +0700

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { InAppAACard } from "@/components/inAppAACard";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <main className="container mx-auto mt-8 p-4">
+        <InAppAACard />
+      </main>
+    </div>
+  );
+}
